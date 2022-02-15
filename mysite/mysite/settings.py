0ca5fd45af7294d5617537533a7157c576777efc
Mar-Nb <m.niombela@cfa-insta.fr>
2022-02-15 11:49:13 +0100

    Modifs CORS 2

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,19 +38,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
-    "myAPI.apps.MyapiConfig",
     "corsheaders",
+    "myAPI.apps.MyapiConfig",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -128,5 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:4200"
+    "http://127.0.0.1:4200",
 ]
